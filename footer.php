3d5d97bd8a013641720473364dbf9db1b5d0ae51
Lucas <lucas98fernando@gmail.com>
2020-09-12 12:42:05 -0300

    Visual changes and improvements in responsiveness

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <!--Footer-->
 <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
 	<div class="container text-center">
-      <small>Copyright &copy; Games Wallpapers | Todos os direitos reservados.</small><br>
-    </div>
+    <small id="footer">Copyright &copy; Games Wallpapers | Todos os direitos reservados.</small><br>
+  </div>
 </footer>
--- index.php
+++ index.php
@@ -18,84 +18,83 @@
 </head>
 <body>
 
-<!--Navbar-->
-	<?php include './header.php'; ?>
-<!--End Navbar-->
+	<!--Navbar-->
+		<?php include './header.php'; ?>
+	<!--End Navbar-->
 
-<!-- Header -->
-	<div class="header">
-	  <h1>Games Wallpapers</h1>
-	  <p>Você pode aquirir de graça todos esses incríveis papéis de parede, divirta-se!</p>
+	<!-- Header -->
+		<div class="header">
+			<!-- Header here -->
+		</div>
+
+	<!--Slideshow-->
+	<div id="demo" class="carousel slide" data-ride="carousel">
+
+	<!-- Indicators -->
+		<ul class="carousel-indicators">
+			<li data-target="#demo" data-slide-to="0" class="active"></li>
+			<li data-target="#demo" data-slide-to="1"></li>
+			<li data-target="#demo" data-slide-to="2"></li>
+		</ul>
+	
+	<!-- The slideshow -->
+		<div class="carousel-inner">
+			<div class="carousel-item active" data-interval="3000">
+				<img src="./img/bf.jpg" alt="Battlefield 4">
+				<div class="carousel-caption d-none d-md-block">
+					<h4>Battlefield 4</h4>
+				</div>
+			</div>
+			<div class="carousel-item" data-interval="3000">
+				<img src="./img/tombraider.jpg" alt="Tomb Raider">
+				<div class="carousel-caption d-none d-md-block">
+					<h4>Tomb Raider</h4>
+				</div>
+			</div>
+			<div class="carousel-item" data-interval="3000">
+				<img src="./img/batman.jpg" alt="Batman Arkham Night">
+				<div class="carousel-caption d-none d-md-block">
+					<h4>Batman Arkham Night</h4>
+				</div>
+			</div>
+		</div>
+	
+	<!-- Left and right controls -->
+		<a class="carousel-control-prev" href="#demo" data-slide="prev">
+			<span class="carousel-control-prev-icon"></span>
+		</a>
+		<a class="carousel-control-next" href="#demo" data-slide="next">
+			<span class="carousel-control-next-icon"></span>
+		</a>
 	</div>
 
-<!--Slideshow-->
-<div id="demo" class="carousel slide" data-ride="carousel">
+	<br>
 
- <!-- Indicators -->
-  	<ul class="carousel-indicators">
-    	<li data-target="#demo" data-slide-to="0" class="active"></li>
-    	<li data-target="#demo" data-slide-to="1"></li>
-    	<li data-target="#demo" data-slide-to="2"></li>
-  	</ul>
-  
- <!-- The slideshow -->
-  	<div class="carousel-inner">
-    	<div class="carousel-item active" data-interval="3000">
-      		<img src="https://wallpaperaccess.com/full/2994.jpg" alt="Battlefield 4">
-      		<div class="carousel-caption d-none d-md-block">
-    			<h4>Battlefield 4</h4>
-  			</div>
-    	</div>
-    	<div class="carousel-item" data-interval="3000">
-      		<img src="https://images6.alphacoders.com/423/thumb-1920-423181.jpg" alt="Tomb Raider">
-      		<div class="carousel-caption d-none d-md-block">
-    			<h4>Tomb Raider</h4>
-  			</div>
-    	</div>
-    	<div class="carousel-item" data-interval="3000">
-      		<img src="https://wallpapermemory.com/uploads/532/batman-arkham-knight-wallpaper-1080p-174099.jpg" alt="Batman Arkham Night">
-      		<div class="carousel-caption d-none d-md-block">
-    			<h4>Batman Arkham Night</h4>
-  			</div>
-    	</div>
-  	</div>
-  
- <!-- Left and right controls -->
-  	<a class="carousel-control-prev" href="#demo" data-slide="prev">
-    	<span class="carousel-control-prev-icon"></span>
-  	</a>
-  	<a class="carousel-control-next" href="#demo" data-slide="next">
-    	<span class="carousel-control-next-icon"></span>
-  	</a>
-</div>
+	<!-- Photo Grid -->
+		<div class="row"> 
+			<div class="column">
+				<img src="./img/batman1.jpg" style="width:100%">
+				<img src="./img/dark.jpg" style="width:100%">
+			</div>
+			<div class="column">
+				<img src="./img/tw3.jpg" style="width:100%">
+				<img src="./img/spider.png" style="width:100%">
+			</div>  
+			<div class="column">
+				<img src="./img/car.jpg" style="width:100%">
+				<img src="./img/lara.jpg" style="width:100%">
+			</div>
+			<div class="column">
+				<img src="./img/mario.jpg" style="width:100%">
+				<img src="./img/hzd.jpg" style="width:100%">
+			</div>
+		</div>
 
-<br>
+	<br>
 
-<!-- Photo Grid -->
-	<div class="row"> 
-  		<div class="column">
-    		<img src="https://comicvine1.cbsistatic.com/uploads/original/11127/111278592/5475050-batman.jpg" style="width:100%">
-    		<img src="https://images.wallpaperscraft.com/image/dark_souls_ii_dark_souls_warrior_knight_from_software_namco_bandai_games_98197_1920x1080.jpg" style="width:100%">
-  		</div>
-  		<div class="column">
-    		<img src="https://i.pinimg.com/originals/2a/65/6b/2a656b330532432caf1742c8586f7fff.jpg" style="width:100%">
-    		<img src="https://www.gamerview.com.br/wp-content/uploads/2018/04/General-1920x1080-spider-Spider-Man-video-games-wallpaper-wp3805861.png" style="width:100%">
-  		</div>  
-  		<div class="column">
-    		<img src="https://s1.1zoom.me/b5050/39/Nissan_Skyline_Concept_452430_1920x1080.jpg" style="width:100%">
-    		<img src="https://images5.alphacoders.com/521/521938.jpg" style="width:100%">
-  		</div>
-  		<div class="column">
-    		<img src="https://wallpapermemory.com/uploads/145/super-mario-galaxy-background-1080p-421329.jpg" style="width:100%">
-    		<img src="https://images7.alphacoders.com/614/614656.jpg" style="width:100%">
-  		</div>
-	</div>
-
-<br>
-
-<!--Footer-->
-<?php include './footer.php'; ?>
-<!--End Footer-->
+	<!--Footer-->
+	<?php include './footer.php'; ?>
+	<!--End Footer-->
 
 </body>
 </html>
